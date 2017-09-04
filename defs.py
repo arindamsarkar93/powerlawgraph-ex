9638cce5370413471b0be07a34230b1423494b08
Arindam Sarkar <93rockingful@gmail.com>
2017-09-04 12:46:17 +0530

    added data, test runs

--- defs.py
+++ defs.py
@@ -3,5 +3,5 @@ eps = 1e-20
 exp = np.exp
 log = lambda x: np.log(x + eps)
 
-DATA_ROOT = '/home/mlg/stonecold/codes/python/random_graph/bgrg/data'
-SAVE_ROOT = '/home/mlg/stonecold/codes/python/random_graph/bgrg/results'
+DATA_ROOT = '/home/arindam/code/powerlawgraph/data'
+SAVE_ROOT = '/home/arindam/code/powerlawgraph/results'
