17cb35896ef8d8dd132199b8d4b969a8332b272f
mysticprince <93rockingful@gmail.com>
2017-12-25 14:08:52 +0530

    fake data test

---
+++ stan_implem_fakedata.py
@@ -0,0 +1,105 @@
+#stan implementation of "power law simple graphs paper"
+from __future__ import division
+import numpy as np
+import statsmodels.api as sm
+import pystan
+from scipy.stats import uniform, norm
+import pickle
+
+N = 10;
+
+data = {};
+data['N'] = N;
+X = np.random.randint(2,size=(N,N)); #adjacency matrix
+data['X'] = X;
+
+beta = 1.0;
+C_n = N**beta;
+
+data['beta'] = beta;
+data['C_n'] = C_n;
+
+#model
+stan_code="""
+functions{
+	real BFRY_lpdf(real w, real alpha){
+		real eps = 1e-20; //small constant to avoid log(0)
+		//By def of BFRY
+		real lpdf = log(alpha + eps) - lgamma(1.0 - alpha) - (alpha + 1.0)*log(w + eps) + log(1 - exp(-w) + eps );
+
+		return lpdf;
+	}
+
+	real tBFRY_lpdf(real w, real alpha, real C_n){
+		//truncation
+		real eps = 1e-20; //small constant to avoid log(0)
+		if(w > C_n)
+			return log(eps); //almost 0 prob.
+
+		else
+			return BFRY_lpdf(w|alpha);
+	}
+
+	//lpdf of generalized random graph as outlined in the paper
+	real grg_lpdf(matrix X, matrix r, int N){
+		real log_Gr = 0.0;
+		real log_x_r = 0.0;
+		real temp = r[1,1];
+
+		for(i in 1:N){
+			for(j in 1:N){
+				log_Gr += log(1+r[i,j]);
+				log_x_r += X[i,j] * log(r[i,j]);
+			}
+		}
+
+		return (-log_Gr + log_x_r);
+	}
+}
+
+data{
+	int<lower=0> N;
+	matrix[N,N] X;
+	real C_n;
+	real beta;
+}
+
+parameters{
+	real<lower=0, upper=0.9999> alpha; //parameter of distribution
+	vector<lower=0>[N] w; //scalar embedding of graph rep. by X
+}
+
+transformed parameters{
+	vector[N] u;
+	matrix[N,N] r;
+	real L;
+
+	L = sum(w);
+	u = w/sqrt(L);
+
+	r = u * u'; //uu^T
+}
+
+model{
+	for(i in 1:N){
+		w[i] ~ tBFRY(alpha,C_n);
+	}
+
+	X ~ grg(r,N);
+}
+
+generated quantities{
+	real log_lik;
+	//likelihood eval
+
+	log_lik = grg_lpdf(X|r,N);
+}
+""";
+
+#Inference
+fit = pystan.stan(model_code = stan_code, data = data, iter=1000, chains = 4, n_jobs=1, verbose = False);
+
+log_lik = fit.extract('log_lik')['log_lik'];
+
+print "log-likelihood:", np.mean(log_lik);
+
